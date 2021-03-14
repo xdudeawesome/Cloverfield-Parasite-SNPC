025b3a730b9ebc557bc4327d68b904e19e401767
X dude Awesome <47611998+xdudeawesome@users.noreply.github.com>
2021-03-14 18:16:11 -0500

    Add files via upload

--- lua/entities/sent_killicon/init.lua
+++ lua/entities/sent_killicon/init.lua
@@ -1,1 +1,12 @@
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
 
+include('shared.lua')
+
+function ENT:Initialize()
+	self:SetModel( "models/props_junk/watermelon01_chunk02c.mdl" )
+	self:SetColor( 255, 255, 255, 0 )
+end
+
+function ENT:Think()
+end 

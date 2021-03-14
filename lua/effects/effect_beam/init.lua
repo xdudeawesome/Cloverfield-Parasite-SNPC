e5b8d165f2751b50cce4538023a5f739c611c416
X dude Awesome <47611998+xdudeawesome@users.noreply.github.com>
2021-03-14 18:07:25 -0500

    Add files via upload

--- lua/effects/effect_beam/init.lua
+++ lua/effects/effect_beam/init.lua
@@ -1,1 +1,17 @@
 
+local mat = Material('particle/bendibeam')
+
+function EFFECT:Init( effectdata )
+	self.start = effectdata:GetStart()
+	self.posend = effectdata:GetOrigin()
+	self:SetPos( self.start )
+end 
+
+function EFFECT:Think()
+end
+
+
+function EFFECT:Render()
+	render.SetMaterial( mat )
+	render.DrawBeam(self.start, self.posend, 2, 0, 1, Color(255, 0, 0, 255))
+end

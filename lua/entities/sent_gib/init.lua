483e65d2ba3924f4c3e9bb5fa2bc5e3dedff023b
X dude Awesome <47611998+xdudeawesome@users.noreply.github.com>
2021-03-14 18:14:48 -0500

    Add files via upload

--- lua/entities/sent_gib/init.lua
+++ lua/entities/sent_gib/init.lua
@@ -1,1 +1,19 @@
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
 
+include('shared.lua')
+
+function ENT:Initialize()
+	self:PhysicsInit( SOLID_VPHYSICS )
+	self:SetMoveType( MOVETYPE_VPHYSICS )
+	self:SetSolid( SOLID_CUSTOM )
+	self:SetHealth(1)
+
+	local phys = self.Entity:GetPhysicsObject()
+	if (phys:IsValid()) then
+		phys:Wake()
+	end
+end
+
+function ENT:Think()
+end 
